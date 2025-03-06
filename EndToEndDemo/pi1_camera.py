c8c7b9c464d895f5778210aac456d8c881d9ca19
riverandrew <riverandrew16@gmail.com>
2025-03-06 19:01:20 +0000

    updated pi1 pi1_camera

--- EndToEndDemo/pi1_camera.py
+++ EndToEndDemo/pi1_camera.py
@@ -4,19 +4,18 @@ import time
 import subprocess
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600, transform=Transform(hflip=1))
-picam2.start()
+#picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600, transform=Transform(hflip=1))
+#picam2.start()
 
 # Define the GStreamer pipeline for streaming
-gst_pipeline = (
-    "libcamerasrc ! video/x-raw,width=800,height=600,framerate=30/1 ! "
-    "videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! "
-    "rtph264pay config-interval=1 pt=96 ! "
-    "udpsink host=192.168.1.102 port=5000"
+libcamera_vid_cmd = (
+    "libcamera-vid --inline --width 800 --height 600 --framerate 30 "
+    "--codec h264 --bitrate 500000 --profile baseline --inline "
+    "--output udp://192.168.1.102:5000"
 )
 
 # Start the GStreamer pipeline
-subprocess.Popen(gst_pipeline, shell=True)
+subprocess.Popen(libcamera_vid_cmd, shell=True)
 
 while (True):
     time.sleep(1)
