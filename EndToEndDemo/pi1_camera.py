974094fed8c28f4141024680206c1fa59a4f406c
riverandrew <riverandrew16@gmail.com>
2025-03-06 19:22:11 +0000

    updated pi1 camera

--- EndToEndDemo/pi1_camera.py
+++ EndToEndDemo/pi1_camera.py
@@ -1,21 +1,19 @@
-from picamera2 import Picamera2, Preview
-from libcamera import Transform
-import time
 import subprocess
-
-picam2 = Picamera2()
-#picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600, transform=Transform(hflip=1))
-#picam2.start()
+import time
 
 # Define the GStreamer pipeline for streaming
 libcamera_vid_cmd = (
     "libcamera-vid --inline --width 800 --height 600 --framerate 30 "
     "--codec h264 --bitrate 500000 --profile baseline --inline "
+    "--timeout 0 "
     "--output udp://192.168.1.102:5000"
 )
 
-# Start the GStreamer pipeline
-subprocess.Popen(libcamera_vid_cmd, shell=True)
+# Start the GStreamer pipeline and capture errors
+process = subprocess.Popen(libcamera_vid_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-while (True):
+
+# Keep the script alive
+while True:
     time.sleep(1)
+    print("Running...")
