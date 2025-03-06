40dce03d09725da4174857984f8fdae341a2d892
riverandrew <riverandrew16@gmail.com>
2025-03-06 19:30:26 +0000

    Reverted changes to pi1_camera.py

--- EndToEndDemo/pi1_camera.py
+++ EndToEndDemo/pi1_camera.py
@@ -3,9 +3,10 @@ import time
 
 # Define the GStreamer pipeline for streaming
 libcamera_vid_cmd = (
-    """libcamera-vid --inline --width 800 --height 600 --framerate 30 \
-  --codec h264 --bitrate 500000 --profile baseline --timeout 0 \
-  --output - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=192.168.1.102 port=5000"""
+    "libcamera-vid --inline --width 800 --height 600 --framerate 30 "
+    "--codec h264 --bitrate 500000 --profile baseline --inline "
+    "--timeout 0 "
+    "--output udp://192.168.1.102:5000"
 )
 
 # Start the GStreamer pipeline and capture errors
