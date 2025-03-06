9de2ec0bfa6d1d6085da63ac3ab21c8a7f94b298
riverandrew <riverandrew16@gmail.com>
2025-03-06 19:43:19 +0000

    updated p2_camera_receiver

--- EndToEndDemo/p2_camera_receiver.py
+++ EndToEndDemo/p2_camera_receiver.py
@@ -3,7 +3,7 @@ import subprocess
 # Define the GStreamer pipeline for receiving and displaying the video stream
 gst_pipeline = (
     "gst-launch-1.0 udpsrc port=5000 caps=\"application/x-rtp, media=video, clock-rate=90000, encoding-name=H264, payload=96\" ! "
-    "rtph264depay ! avdec_h264 ! videoconvert ! autovideosink"
+    "rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! autovideosink"
 )
 
 # Start the GStreamer pipeline
