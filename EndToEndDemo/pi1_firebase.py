a03c70f136ea9497e1a430d7fc2dff571a7fd941
riverandrew <riverandrew16@gmail.com>
2025-03-07 15:50:43 +0000

    Updated pi1_firebase and venv folder

--- EndToEndDemo/pi1_firebase.py
+++ EndToEndDemo/pi1_firebase.py
@@ -11,7 +11,6 @@ config = {
   "storageBucket": "lsysc-3010-project-l2-g6.firebasestorage.app"
 }
 
-sense = SenseHat()
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 dataset = "ui_test_messages"
@@ -19,39 +18,11 @@ dataset = "ui_test_messages"
 # Write random numbers to database
 def writeData():
     key = 1
-    sensorData = "test pi 1"
+    sensorData = "Test pi 1"
     db.child(dataset).child(key).set(sensorData)
     time.sleep(1)
-    
-
-def readData():
-  # Returns the entry as an ordered dictionary (parsed from json)
-  mySensorData = db.child(dataset).get()
-
-  print("Parent Key: {}".format(mySensorData.key()))
-  print("Parent Value: {}\n".format(mySensorData.val()))
-
-  # Returns the dictionary as a list
-  mySensorData_list = mySensorData.each()
-  # Takes the last element of the list
-  lastDataPoint = mySensorData_list[-1]
-
-  print("Child Key: {}".format(lastDataPoint.key()))
-  print("Child Value: {}\n".format(lastDataPoint.val()))
-  
-#def clearData():
-    #try:
-        # Reference the dataset and remove all data under it
-       # db.child(dataset).remove()
-       # print(f"All data under '{dataset}' has been cleared.")
-   # except Exception as e:
-        # print(f"An error occurred while clearing data: {e}")
 
 def main ():
-   #clearData()
     writeData()
-    readData()
-
-
 main()
 
