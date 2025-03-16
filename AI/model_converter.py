8919b3ed51582fbc026b60cf1b9d8936c7aa1e68
Kyle Mathias <94811777+kylecmathias@users.noreply.github.com>
2025-03-16 15:08:19 -0400

    added a test script to check if the model works

--- AI/model_converter.py
+++ AI/model_converter.py
@@ -5,5 +5,5 @@ model = tf.keras.models.load_model("asl_model.h5")
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-with open("asl_model.tflite", "wb") as f:
+with open("model.tflite", "wb") as f:
     f.write(tflite_model)
