281e8e0ea3de15fc7cc4fa7717f064441cb4fa0f
kylecmathias <kylecraigmathias@gmail.com>
2025-03-16 14:05:24 -0400

    edited model_converter

--- AI/model_converter.py
+++ AI/model_converter.py
@@ -1,12 +1,9 @@
 import tensorflow as tf
 
-# Load trained Keras model
 model = tf.keras.models.load_model("asl_model.h5")
 
-# Convert to TensorFlow Lite format
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-# Save the converted model
 with open("asl_model.tflite", "wb") as f:
     f.write(tflite_model)
