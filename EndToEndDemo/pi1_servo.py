00ddac0e5b7311269f56d1f18c315f43db8a1f0c
riverandrew <riverandrew16@gmail.com>
2025-03-17 09:59:27 -0400

    Created Unit Test demo folder and servo Test

--- EndToEndDemo/pi1_servo.py
+++ EndToEndDemo/pi1_servo.py
@@ -1,7 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
+
+
 servoPIN = 18
+moveUp = false
+moveDown = false
+
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
@@ -16,12 +23,18 @@ def set_angle(angle):
 
 try:
     while True:
-        #set_angle(0)
-        #time.sleep(1)
-        #set_angle(180)
-        #time.sleep(1)
-        set_angle(90)
-        time.sleep(1)
+        if (movUp){
+            set_angle(180)
+            moveUp = false
+            time.sleep(1)
+        }
+        else if (moveDown){
+            set_angle(0)
+            moveDown = false
+            time.sleep(1)
+        }
+
+
 except KeyboardInterrupt:
     pass
 finally:
