0a920d01226e8c60be556999df26d9a9c2d34478
Kyle Mathias <kylecraigmathias@gmail.com>
2025-03-29 12:01:10 -0400

    Update config.py

--- src/camera/config.py
+++ src/camera/config.py
@@ -2,6 +2,10 @@ VID_CMD = (
     "libcamera-vid --inline --width 800 --height 600 --framerate 30 --codec h264 --bitrate 500000 -t 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay ! udpsink host=192.168.1.102 port=5000"
 )
 
+TEST_PIPELINE = "libcamera-vid -t 0 --width 640 --height 480 --framerate 30 --codec h264 --inline -o - | \
+gst-launch-1.0 fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! \
+udpsink host=192.168.1.102 port=5000"
+
 FB_CERT = "/home/andrewrivera/sysc3010-project-l2-g6/config/interprePi access key.json"
 
 FB_URL = {"databaseURL": "https://sysc-3010-project-l2-g6-default-rtdb.firebaseio.com"}
