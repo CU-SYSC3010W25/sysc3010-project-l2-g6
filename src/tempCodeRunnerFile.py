ceb468365fc6481dfbe9ba207df680ca0a5b865a
Kyle Mathias <94811777+kylecmathias@users.noreply.github.com>
2025-03-29 12:21:13 -0400

    changed the receiver pipeline

---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+import time
+
+from processorFull.Processor import Processor
+
+def main():
+    processor = Processor()
+
+    while True:
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
