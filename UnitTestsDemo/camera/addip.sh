1a1b509ac070b710c6dad5a92231c0c8500adfda
riverandrew <riverandrew16@gmail.com>
2025-04-04 19:24:53 -0400

    code clean up

--- UnitTestsDemo/camera/addip.sh
+++
@@ -1,1 +0,0 @@
-sudo ip addr add 192.168.1.101/24 dev eth0 
--- UnitTestsDemo/run_camera.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from camera.Camera import Camera
-
-async def main():
-    camera = Camera()
-    await camera.run()  # Camera handles async internally
-
-asyncio.run(main())
- 
